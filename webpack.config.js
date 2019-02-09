2b37f0fb764050c93e1c68540989b46499596cc4
leah123 <leah@sixthnet.com>
2019-02-09 22:15:30 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,14 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const resolve = dir => path.join(__dirname, '..', dir)
-
+//const {join:pathJoin}  = {join:fun} 所以pathJoin就是哪个方法了 ；
+//path.join将该路径片段进行拼接   path.resolvee将以/开始的路径片段作为根目录，在此之前的路径将会被丢弃，就像是在terminal中使用cd命令一样。并且返回绝对路径
+//'.'指向执行文件的目录  __dirname 总是指向被执行 js 文件的绝对路径
 module.exports = (env, argv) => ({
-	// entry: resolve('src/main.js'),
 	entry:path.resolve('src/main.js'),
 	output: {
-		filename: '[name].[hash:5].js',
-		path: resolve('dist')
+		filename: '[name].[hash:5].js',// 输出文件名字的格式([name]对应entry的键名(app))
+		path: path.resolve('dist'),
 	},
 	optimization: {
 		splitChunks: {
@@ -33,7 +34,7 @@ module.exports = (env, argv) => ({
 			{
 				test: /\.(css|scss)$/,
 				use: [
-					argv.mode === 'development' ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+					argv.mode === 'development' ? 'vue-style-loader' : MiniCssExtractPlugin.loader, // 使用vue-style-loader直接插入到style标签中
 					'css-loader',
 					'sass-loader',
 					{
@@ -64,8 +65,8 @@ module.exports = (env, argv) => ({
 	plugins: [
 		new VueLoaderPlugin(),
 		new HtmlWebpackPlugin({
-			// template: resolve('index.html')
-			template:'index.html'
+			template:'index.html',// 指定模板html文件
+			filename: 'index.html' // 输出的html文件名称
 		}),
 		new MiniCssExtractPlugin({
 			filename: '[name].css'
